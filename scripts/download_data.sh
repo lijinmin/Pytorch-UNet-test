d59b0ce792c2520212e2a4580a2e2b704fa500dc
lijinmin <lijinmin3903@126.com>
2025-06-25 16:16:27 +0800

    m

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -16,12 +16,12 @@ pip install kaggle --upgrade
 
 kaggle competitions download -c carvana-image-masking-challenge -f train_hq.zip
 unzip train_hq.zip
-mv train_hq/* data/imgs/
+mv train_hq/* ../data/imgs/
 rm -d train_hq
 rm train_hq.zip
 
 kaggle competitions download -c carvana-image-masking-challenge -f train_masks.zip
 unzip train_masks.zip
-mv train_masks/* data/masks/
+mv train_masks/* ../data/masks/
 rm -d train_masks
 rm train_masks.zip
